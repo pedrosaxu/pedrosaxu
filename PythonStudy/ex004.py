9425ec3edffef2751b8ce7688cee46fd7ddf9325
Julia Nunes Pedrosa <julianun3sp@gmail.com>
2022-04-04 08:13:50 -0300

    Exercise 4 🐛

---
+++ PythonStudy/ex004.py
@@ -0,0 +1,9 @@
+var = input('Type something: ')
+
+print(f'Its type is: {type(var)}')
+print(f'It has only spaces? {var.isspace()}')
+print(f'Is it numeric? {var.isnumeric()}')
+print(f'Is it alphanumeric? {var.isalpha()}')
+print(f'Is it uppercased? {var.isupper()}')
+print(f'Is it lowercased? {var.islower()}')
+print(f'Is it capitalized? {var.istitle()}')
--- PythonStudy/readme.md
+++ PythonStudy/readme.md
@@ -1,3 +1,5 @@
 - <strong>ex001:</strong> Print "Hello world!" on the screen.
 - <strong>ex002:</strong> Read a person's name and welcomes this person.
 - <strong>ex003:</strong> Read two numbers and sum them up.
+- <strong>ex004:</strong> Dissecting a variable.
+
